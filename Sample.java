0e860716c86da44f2bb2a70d7ea21955bb09cbdf
gajankiramadevi <gadhikiramadevi123@gmail.com>
2021-11-10 09:58:54 +0530

    java

---
+++ Sample.java
@@ -0,0 +1,13 @@
+ class Sample{
+    public static void main(String args[]){
+    //int i,j, n=4;
+     for(int i=0;i<5;i++)
+     {
+         for( int j=0;j<=i;j++)
+         {
+            System.out.println("*");
+         }
+         System.out.println();
+     }
+    }
+}
